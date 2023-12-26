a774e49f1aefab2ab858b5604a2181beeffdcc14
neeraj26777 <154912239+neeraj26777@users.noreply.github.com>
2023-12-26 22:12:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings: ["Autocad Designer . . ." , "3D Designer . . ." , "Structure Designer . . .","Interior Designer. . . "],
+    typeSpeed: 70,
+    backSpeed: 50,
+    backDelay: 1000,
+    loop: true
+})
